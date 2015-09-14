b8d4d2f90ca0d3fe7def821254534faab42b6e02
SLeonov <ragioniere1980@gmail.com>
2015-09-14 22:25:13 +0300

    yet commit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,9 +10,24 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
-Route::get('/', 'WelcomeController@index');
-
+Route::get('/', 'PagesController@contact');
 Route::get('contact', 'PagesController@contact');
 
 Route::get('about', 'PagesController@about');
+
+Route::resource('articles', 'ArticlesController');
+
+Route::controllers([
+    'auth' => 'Auth\AuthController',
+    'password' =>'Auth\PasswordController'
+]);
+
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+
